33f14051bdfa94285ed74595acd8d1f5d9d8a6c7
Max Chou <max9159@gmail.com>
2024-11-11 23:51:21 +0800

    Update api port

--- backend/app/server.js
+++ backend/app/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const axios = require('axios');
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 const config = require('./config'); // Load configuration file
 const cors = require('cors');
 
